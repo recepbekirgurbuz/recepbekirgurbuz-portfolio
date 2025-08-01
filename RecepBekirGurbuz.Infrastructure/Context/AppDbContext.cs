d88396a4f2bb6da47f22b977e032da60cb746a23
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2025-08-01 10:58:59 +0300

    Remove duplicate AppDbContext and fix namespace conflict

---
+++ RecepBekirGurbuz.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecepBekirGurbuz.Core.Entities;
+
+namespace RecepBekirGurbuz.Infrastructure.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
