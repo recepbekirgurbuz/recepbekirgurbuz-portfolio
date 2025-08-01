05ba6d9ed63f1ec1fd6d2883619c08ae2b29e1be
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2025-08-01 19:48:41 +0300

    Add UserController with CRUD endpoints and test via Swagger

--- RecepBekirGurbuz/Program.cs
+++ RecepBekirGurbuz/Program.cs
@@ -1,29 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using RecepBekirGurbuz.Infrastructure.Context;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
-// EF Core: Add DbContext with SQL Server connection
+// Swagger ekle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// EF Core SQL Server baðlantýsý
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Swagger / OpenAPI
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
