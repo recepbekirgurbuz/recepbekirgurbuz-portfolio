10d3fdbd6b13a19fec9605762ac1037118ab8a96
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2025-08-03 11:39:10 +0300

    Make CreateUserCommand implement IRequest<int> for MediatR compatibility

---
+++ RecepBekirGurbuz.Application/Handlers/Users/CreateUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace RecepBekirGurbuz.Application.Handlers.Users
+{
+    public class CreateUserCommand : IRequest<int>
+    {
+        public required string FullName { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+    }
+}
